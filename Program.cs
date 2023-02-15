926967fa1331331df8086df84033b8281cc00dd0
Jbautistacarmona <124100561+Jbautistacarmona@users.noreply.github.com>
2023-02-15 13:37:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.tómbola_de_lotería_nacional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bienvenido a la tómbola de lotería!");//mensaje de benvenida al usuario.
+            Console.WriteLine("Presione Enter para seleccionar los números ganadores...");
+            Console.ReadLine();
+
+            Random rnd = new Random(); //random generador
+            int firstPlace = rnd.Next(1, 100);//para el primer numero
+            int secondPlace = rnd.Next(1, 100);//segundo numero generado
+            int thirdPlace = rnd.Next(1, 100);//ultimo nuevo generado,
+
+            Console.WriteLine("Los números ganadores son: ");
+            Console.WriteLine("1er lugar: " + firstPlace);
+            Console.WriteLine("2do lugar: " + secondPlace);
+            Console.WriteLine("3er lugar: " + thirdPlace);
+            Console.ReadLine();//imprimir resultado en pantalla
+        }
+    }
+}
